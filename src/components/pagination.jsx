ee7b038184fd2abd1317756d366c2110719d7b60
KovaletsV <kovalets2902@gmail.com>
2021-09-07 22:41:55 +0900

    pagination

---
+++ src/components/pagination.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import _ from "lodash";
+
+const Pagination = (props) => {
+  const { itemsCount, pageSize } = props;
+  const pageCount = itemsCount / pageSize;
+  const pages = _.range(1, pageCount + 1);
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li className="page-item" key={page}>
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 import User from "./user";
+import Pagination from "./pagination";
 
 const Users = ({ users, onDelete, onToggleMark }) => {
   return (
     <>
-      <table className="table align-middle">
-        <thead>
-          <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col">Избранное</th>
-            <th scope="col" />
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <User
-              key={user._id}
-              onDelete={onDelete}
-              onToggleMark={onToggleMark}
-              {...user}
-            />
-          ))}
-        </tbody>
-      </table>
+      {users.length > 0 && (
+        <table className="table align-middle">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col">Избранное</th>
+              <th scope="col" />
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <User
+                key={user._id}
+                onDelete={onDelete}
+                onToggleMark={onToggleMark}
+                {...user}
+              />
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
