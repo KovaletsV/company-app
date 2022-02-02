df530f39d465ad5f4c75200fab7be99c81aa23a7
KovaletsV <kovalets2902@gmail.com>
2022-02-02 15:53:48 +0900

    ffff

--- src/hooks/useQuality.jsx
+++ src/hooks/useQuality.jsx
@@ -2,61 +2,61 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { toast } from "react-toastify";
 import qualityService from "../services/quality.service";
-// import professionService from "../services/profession.service";
 
 const QualitiesContext = React.createContext();
 
 export const useQualities = () => {
-  return useContext(QualitiesContext);
+    return useContext(QualitiesContext);
 };
 
 export const QualitiesProvider = ({ children }) => {
-  const [qualities, setQualities] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [qualities, setQualities] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    getQualityList();
-  }, []);
+    useEffect(() => {
+        getQualityList();
+    }, []);
 
-  useEffect(() => {
-    if (error !== null) {
-      toast(error);
-      setError(null);
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
+
+    function getQualities(id) {
+        return qualities.filter(q => id.includes(q._id));
     }
-  }, [error]);
 
-  function getQualities(id) {
-    return qualities.filter((q) => id.includes(q._id));
-  }
-
-  async function getQualityList() {
-    try {
-      const { content } = await qualityService.get();
-      setQualities(content);
-      setLoading(false);
-    } catch (error) {
-      errorCatcher(error);
+    async function getQualityList() {
+        try {
+            const { content } = await qualityService.get();
+            setQualities(content);
+            setLoading(false);
+        } catch (error) {
+            errorCatcher(error);
+        }
     }
-  }
 
-  function errorCatcher(error) {
-    const { message } = error.response.data;
-    setError(message);
-    setLoading(false);
-  }
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+        setLoading(false);
+    }
 
-  // console.log("QualitiesProvider работает", qualities, isLoading);
-  return (
-    <QualitiesContext.Provider value={{ isLoading, qualities, getQualities }}>
-      {children}
-    </QualitiesContext.Provider>
-  );
+    return (
+        <QualitiesContext.Provider
+            value={{ isLoading, qualities, getQualities }}
+        >
+            {children}
+        </QualitiesContext.Provider>
+    );
 };
 
 QualitiesProvider.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ])
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
 };
--- src/services/http.service.js
+++ src/services/http.service.js
@@ -5,25 +5,28 @@ import config from "../config.json";
 //  адрес по-умолчанию
 axios.defaults.baseURL = config.apiEndpoint;
 
-axios.interceptors.response.use((res) => {
-  // toast.info("Данные успешно изменены!")
-  return res;
-}, function (error) {
-  // console.log("interceptor");
-  const expectedErrors = error.response && error.response.status >= 400 && error.response.status < 500;
-  if (!expectedErrors) {
-    console.log(error);
-    toast.error(`Произошла ошибка.\nКоманда работает над исправлением.\nПопробуйте позже.`);
-    // toast("Unexpected error");
-  }
-  return Promise.reject(error);
-});
+axios.interceptors.response.use(
+    res => {
+        return res;
+    },
+    function (error) {
+        const expectedErrors =
+            error.response &&
+            error.response.status >= 400 &&
+            error.response.status < 500;
+        if (!expectedErrors) {
+            console.log(error);
+            toast.error("Error");
+        }
+        return Promise.reject(error);
+    }
+);
 
 const httpService = {
-  get: axios.get,
-  post: axios.post,
-  put: axios.put,
-  delete: axios.delete
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete
 };
 
 export default httpService;
